299ae70afc75127ef68b97e0229359dcb3c654dc
anggunarida <76078074+anggunarida@users.noreply.github.com>
2020-12-16 07:20:29 +0700

    Add files via upload

---
+++ data/titikacuansemarang_1.js
@@ -0,0 +1,8 @@
+var json_titikacuansemarang_1 = {
+"type": "FeatureCollection",
+"name": "titikacuansemarang_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.412006627347068, -7.094967350031582 ] } }
+]
+}
